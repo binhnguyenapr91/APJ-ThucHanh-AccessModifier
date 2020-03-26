40574950f05646d62c262213faf96bcdc1e9c4b3
binhnguyenapr91 <binhnguyen.apr91@gmail.com>
2020-03-26 08:18:13 +0700

    init commit

---
+++ src/mypack/App.java
@@ -0,0 +1,9 @@
+package mypack;
+import pack.*;
+public class App extends A{
+    public static void main(String[] args) {
+        App obj=  new App();
+        System.out.println(obj.data);
+        obj.msg();
+    }
+}
---
+++ src/pack/A.java
@@ -0,0 +1,7 @@
+package pack;
+public class A {
+    protected int data=0;
+    protected void msg(){
+        System.out.println("Hello Java");
+    }
+}
